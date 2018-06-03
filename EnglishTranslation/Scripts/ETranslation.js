e39faabc75357547996a861f9d5271fd71731b61
hungh <hungh@DESKTOP-CMGSLRI>
2018-06-03 23:12:19 +0800

    Commit 3/6/2018

---
+++ EnglishTranslation/Scripts/ETranslation.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(function () {
+  setInterval("slideSwitch()", 5000);
+});
+
+function gotoHome() {
+
+}
+function gotoAbout() {
+
+}
+function gotoCommtor() {
+
+}
+
+function gotoContact() {
+
+}
+
+function gotoContact() {
+
+}
+function slideSwitch() {
+  var $active = $('#slideshow IMG.active');
+  var $next = $active.next();
+
+  if ($next.length === 0) {
+    $('#1').removeClass();
+    $('#2').removeClass();
+    $('#3').removeClass();
+    $('#1').addClass('active');
+  }
+
+  $active.addClass('last-active');
+
+  $next.css({ opacity: 0.0 })
+    .addClass('active')
+    .animate({ opacity: 1.0 }, 1000, function () {
+      $active.removeClass('active last-active');
+    });
+}
